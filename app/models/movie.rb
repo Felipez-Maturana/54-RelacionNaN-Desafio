c7a1d0388788293c8e26cfd45f66edacce23446f
Felipez-Maturana <felipe.maturana.g@usach.cl>
2017-07-28 19:04:01 -0400

    version entrega

---
+++ app/models/movie.rb
@@ -0,0 +1,5 @@
+class Movie < ApplicationRecord
+  has_many :movie_tags
+  has_many :tags, through: :movie_tags
+  attr_accessor :tags_ids
+end
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+MovieTag.delete_all
+Movie.delete_all
+Tag.delete_all
+
+m = Movie.new(name: "Terminator")
+m.tags << Tag.new(name:"Terrible de buena")
+m.tags << Tag.new(name:"Acción")
+m.save
+
+# m = Movie.new(name: "Terminator")
+# m.tags.build(name:"Terrible de buena")
+# m.tags.build(name:"Acción")
+# m.save
